3ac29319a23153619b4631d300230ac945710551
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-01-13 17:46:53 +0530

    Created chess grid

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+
+#define HEIGHT 800
+#define WEIGHT 800
+
+using std::cout;
+using std::endl;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(HEIGHT, WEIGHT), "SFML works!");
+
+    std::vector<std::vector<sf::RectangleShape>> board(8, std::vector<sf::RectangleShape>(8));
+
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            sf::RectangleShape &cell = board[i][j];
+            cell.setSize({HEIGHT/8, WEIGHT/8});
+            cell.setPosition(j*(HEIGHT/8), i*(WEIGHT/8));
+            if((i+j)%2==0 || (i==0&&j==0))
+                cell.setFillColor(sf::Color::White);
+            else
+                cell.setFillColor(sf::Color::Black);
+        }
+    }
+
 
     while (window.isOpen())
     {
         sf::Event event;
+        window.waitEvent(event);
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
@@ -16,7 +38,11 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        for(auto i:board){
+            for(sf::RectangleShape r:i){
+                window.draw(r);
+            }
+        }
         window.display();
     }
 
