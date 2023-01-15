2ae442926261d9b112dba00ace37ce0745e09b5e
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-01-15 10:35:10 +0530

    added cell selection mechanism

--- board.h
+++ board.h
@@ -18,20 +18,38 @@ struct Pos{
 class Board{
 public:
     std::vector<std::vector<sf::RectangleShape>> board;
+    Pos selected;
 
     Board(){
         this -> board = std::vector<std::vector<sf::RectangleShape>>(8, std::vector<sf::RectangleShape>(8));
+        this->selected = {0, 0};
+
         for(int i=0; i<8; i++){
             for(int j=0; j<8; j++){
                 sf::RectangleShape &cell = this->board[i][j];
                 cell.setSize({HEIGHT/8, WIDTH/8});
                 cell.setPosition(j*(HEIGHT/8), i*(WIDTH/8));
                 if((i+j)%2==0 || (i==0&&j==0)){
-                    cell.setFillColor(sf::Color::White);
+                    cell.setFillColor(sf::Color(125, 148, 93));
                 }else{
-                    cell.setFillColor(sf::Color::Black);
+                    cell.setFillColor(sf::Color(238, 238, 213));
                 }
             }
+        }
+    }
+
+    void cell_select(Pos &pos){
+        if((selected.x+selected.y)%2==0 || (selected.x==0&&selected.y==0)){
+            board[selected.y][selected.x].setFillColor(sf::Color(125, 148, 93));
+        }else{
+            board[selected.y][selected.x].setFillColor(sf::Color(238, 238, 213));
+        }
+
+        selected = pos;
+        if((selected.x+selected.y)%2==0 || (selected.x==0&&selected.y==0)){
+            board[selected.y][selected.x].setFillColor(sf::Color(186, 202, 43));
+        }else{
+            board[selected.y][selected.x].setFillColor(sf::Color(245, 236, 105));
         }
     }
 
@@ -129,6 +147,7 @@ public:
             Pos clicked_pos;
             clicked_pos.x = event.mouseButton.x/(HEIGHT/8);
             clicked_pos.y = event.mouseButton.y/(WIDTH/8);
+            b.cell_select(clicked_pos);
         } 
     }
 
